85eec8634caba015d1de11fe24bea3f7ff24db60
Steven Barash <stevenbarash6@gmail.com>
2018-10-06 14:25:51 -0400

    it works

--- superkid/src/App.js
+++ superkid/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import React, {Component} from 'react'
 import QrReader from 'react-qr-reader'
 import ScanTheQR from './ScanTheQR';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
   render() {
@@ -12,7 +13,11 @@ class App extends Component {
       <div className="parent">
         <div className="qr-scanner">
           <ScanTheQR/>
+
         </div>
+        <Button className="scan-button" variant="contained" color="primary">
+          Scan
+        </Button>
       </div>
     )
   }
---
+++ superkid/src/PatientInfo.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react'
+
+export default class PatientInfo extends Component {
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
